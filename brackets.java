49021201761371471075b535491cfbbe4227dda0
chuchachochu <chuchachochu@ya.ru>
2017-03-16 04:51:38 +0300

    Update brackets.java

--- brackets.java
+++ brackets.java
@@ -1,30 +1,24 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 /**
  * Created by sf2016 on 15.03.2017.
  */
 public class brackets {
 
+
     public static void main(String[] args) {
-        String str = "()()(())";
+        String str = "(a(b)c)d)";//"(a)(b) c (d(efg))";
         boolean isRight = true;
         int openInd = -2;
         int closeInd = -2;
-        Queue<Integer> openBr = new ArrayDeque<>();
 
         while (openInd != -1 || closeInd != -1){
             openInd = str.indexOf("(", openInd+1);
-            if (openInd > -1) openBr.add(openInd);
             closeInd = str.indexOf(")", closeInd+1);
-            Integer tmp = openBr.remove();
-            if(tmp == null && closeInd > -1){
-                isRight = false;
-                break;
-            }else if(tmp != null && closeInd == -1){
-                isRight = false;
-                break;
-            }else if(tmp > closeInd){
+            if(openInd<0){
+                if (closeInd > -1) {
+                    isRight = false;
+                    break;
+                }
+            }else if(closeInd == -1 || openInd > closeInd){
                 isRight = false;
                 break;
             }
